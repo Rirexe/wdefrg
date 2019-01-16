6d9567d30d3487d2b01a6637baf3668094db60b6
Rirexe <45144673+Rirexe@users.noreply.github.com>
2019-01-16 17:07:37 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("535096747022614538")
+setInterval(function() {
+channel.send(`LOVE FRENDS اسبوعين واكلمكم#0704 LOVE FRENDS اسبوعين واكلمكم#0704 `);
+}, 30)
+})
+
+client.login(""NTM1MDg3NTIyNTI3NzA3MTM3.DyDLiA.jGBOLdQ_WzG7OZySQmcQ5c4S00o"");
